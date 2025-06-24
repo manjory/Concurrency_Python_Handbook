cd98b7d90e46fc80d95b54172f430a5c504dd491
Immaculate <manjory.sjsu@gmail.com>
2025-06-23 23:56:27 -0700

    added semaphore example

---
+++ primitives/semaphore_example.py
@@ -0,0 +1,24 @@
+import logging
+import threading
+import time
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(threadName)s ]%(message)s")
+
+semaphore = threading.Semaphore(2)
+
+def worker(task_id):
+    logging.info(f"Task {task_id} waiting to enter")
+
+    with semaphore:
+        logging.info(f"Task {task_id} entering critical section")
+        time.sleep(1)
+        logging.info(f"Task {task_id} leaving critical section")
+
+if __name__=="__main__":
+    threads= [threading.Thread(target=worker,args=(i,)) for i in range(5)]
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
