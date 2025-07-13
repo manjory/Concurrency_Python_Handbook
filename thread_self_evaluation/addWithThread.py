63a63ee9ec70b1f0bf4a163e00a68a9039837581
Immaculate <manjory.sjsu@gmail.com>
2025-07-13 00:57:50 -0700

    thread Add 1 in the critical section and use global variable to share critical section

---
+++ thread_self_evaluation/addWithThread.py
@@ -0,0 +1,46 @@
+"""(READ/WRITE lock)
+1. Write a function to implement critical section. This function should have three actions .
+i). Print the number it’s acting on with the thread ID.
+ii). Add 1 to the number. And,
+iii). Print the number after updating with its thread ID
+2. Use a function call This method from two different time threads 10 times each
+currect implementation expects 21 in response
+"""
+import threading
+from threading import Lock
+
+mutex=Lock()
+shared_number=1
+
+def critical_section(thread_id):
+    global shared_number
+    mutex.acquire()
+    print("number: ",shared_number," thread_id :",thread_id)
+    shared_number+=1
+    print("number: ",shared_number," thread_id :",thread_id)
+    mutex.release()
+    # return number
+
+def call_function(thread_id):
+    # number=1
+    for i in range(10):
+        critical_section(thread_id)
+
+if __name__ == "__main__":
+    thread1=threading.Thread(target=call_function, args=("thread_1",))
+    thread2=threading.Thread(target=call_function,args=("thread_2",))
+    print("thread_1 action start")
+    thread1.start()
+    thread1.join()
+    print("thread_1 action ended")
+    print("--------------------------------------")
+    print("thread_2 action start")
+    thread2.start()
+    thread2.join()
+    print("thread_2 action ended")
+
+
+
+
+
+
--- thread_self_evaluation/critical_section_implementation.py
+++ thread_self_evaluation/critical_section_implementation.py
@@ -1,6 +1,9 @@
 """
  Write multithreaded function to implement a critical section and show the case of critical section
 """
+import threading
+from threading import Thread,Lock
+
 """
 CRITICAL SECTION problem is solved using 2 general approaches:
 1. Per-emptive kernels: It allows the process to be preemted while running in the kernel mode. This comes as a better option with a good Kernel code design
@@ -9,5 +12,36 @@ for processes running in concurrent manner.
 2. Nonpreemptive kernels:  in this mode the Nonpreemptive kernel does not allow a process running in kernel mode to be preempted. A kernel mode process
 will run until it exits the kernel mode, blocks or voluntarily yields control of the CPU. Inherently solves the race condition problems, 
 but runs the risk of hogging the CPU for significantly long time
-
+    
 """
+mutex=Lock()
+
+parkingSpace=10
+def test_and_set(numCars):
+    global parkingSpace
+    mutex.acquire()
+    try:
+        parkingSpace= parkingSpace + numCars
+        print("total number of cars in parking space", parkingSpace)
+    finally:
+        mutex.release()
+
+
+
+
+
+if __name__ == "__main__":
+    n=10
+    threads=[]
+    for i in range(n):
+        thread_handler= threading.Thread(target=test_and_set,args=(2,))
+        thread_handler.start()
+        threads.append(thread_handler)
+        print("\nhandler done")
+    for t in thread_handler:
+        t.join()
+
+
+
+# add a number and print the number before and after. with thread id and thread name, exit
+# wap to add a number to critical section before and after update with thread id
